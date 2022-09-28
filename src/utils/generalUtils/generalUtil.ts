ebb3e387022063d77d15c0a3f4a9a7d01521d0d4
AhmadHddad <AhmadHddad@outlook.com>
2022-09-28 22:54:52 +0300

    renamcing to is Number

--- src/utils/generalUtils/generalUtil.ts
+++ src/utils/generalUtils/generalUtil.ts
@@ -87,7 +87,7 @@ export const isNullOrEmptyString = (value?: string | null) =>
  * If the value is not a number, then it is not a number.
  * @param {any} value - any - The value to be checked
  */
-export const isNumeric = (value: any): value is number =>
+export const isNumber = (value: any): value is number =>
   !isNaN(value - parseFloat(value));
 
 /**
