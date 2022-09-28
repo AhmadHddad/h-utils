f4552bc0f6e29f72974a103fa72f329997334dd9
AhmadHddad <AhmadHddad@outlook.com>
2022-09-28 22:16:32 +0300

    adding style utils

---
+++ src/utils/styleUtils/styleTypes.ts
@@ -0,0 +1,7 @@
+export declare namespace classNames {
+    type Value = string | number | boolean | undefined | null;
+    type Mapping = Record<string, unknown>;
+    interface ArgumentArray extends Array<Argument> {}
+    type Argument = Value | Mapping | ArgumentArray;
+  }
+  
---
+++ src/utils/styleUtils/styleUtils.ts
@@ -0,0 +1,40 @@
+import { classNames } from "./styleTypes";
+
+/**
+ * It takes any number of arguments, and returns a string of all the arguments that are truthy,
+ * separated by spaces.
+ */
+export function classNames(...args: classNames.ArgumentArray): string {
+  const classes = [];
+  const hasOwn = {}.hasOwnProperty;
+
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    if (!arg) continue;
+
+    const argType = typeof arg;
+
+    if (argType === 'string' || argType === 'number') {
+      classes.push(arg);
+    } else if (Array.isArray(arg)) {
+      if (arg.length) {
+        const inner = classNames.apply(null, arg);
+        if (inner) {
+          classes.push(inner);
+        }
+      }
+    } else if (argType === 'object') {
+      if (arg.toString === Object.prototype.toString) {
+        for (let key in arg as {}) {
+          if (hasOwn.call(arg, key) && arg[key]) {
+            classes.push(key);
+          }
+        }
+      } else {
+        classes.push(arg.toString());
+      }
+    }
+  }
+
+  return classes.join(' ');
+}
