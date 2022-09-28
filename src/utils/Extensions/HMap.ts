17aeb83064df22200d528dd9b4ee8e676fc4eb3c
AhmadHddad <AhmadHddad@outlook.com>
2022-09-28 22:15:16 +0300

    adding extension HMap

---
+++ src/utils/Extensions/HMap.ts
@@ -0,0 +1,61 @@
+class HMap<K, V> extends Map<K, V> {
+  static from(obj: {}) {
+    return new HMap(Object.entries(obj));
+  }
+
+  filter = callback => {
+    const filtered = new HMap();
+    this.forEach((value, key, map) => {
+      if (callback(value, key, map)) {
+        filtered.set(key, value);
+      }
+    });
+    return filtered;
+  };
+
+  getValuesArray = () => {
+    return Array.from(this.values());
+  };
+
+  map = callback => {
+    const mapped = new HMap();
+    this.forEach((value, key, map) => {
+      mapped.set(key, callback(value, key, map));
+    });
+    return mapped;
+  };
+
+  mapArray = callback => {
+    const arr = [];
+    this.forEach((v, k) => {
+      arr.push(callback(v, k));
+    });
+    return arr;
+  };
+
+  findValue = callback => {
+    let found;
+    this.forEach((value, key, map) => {
+      if (callback(value, key, map)) {
+        found = value;
+      }
+    });
+    return found;
+  };
+
+  all = callback => {
+    return (
+      this.map(callback)
+        .getValuesArray()
+        .indexOf(false) === -1
+    );
+  };
+
+  any = callback => {
+    return (
+      this.map(callback)
+        .getValuesArray()
+        .indexOf(true) !== -1
+    );
+  };
+}
