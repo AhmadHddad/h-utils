4a512f56dce4e0d41370e75a7dceb0f0502d1617
AhmadHddad <AhmadHddad@outlook.com>
2023-02-21 13:08:13 +0200

    adding regex

---
+++ src/regex/regex.ts
@@ -0,0 +1,6 @@
+export const urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+export const httpURLRegex = /^((http|https|ftp):\/\/)/;
+export const upperCaseRegex = /[A-Z]?[a-z]+|[0-9]+|[A-Z]+(?![a-z])/g;
+export const emailRegex = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+
+
