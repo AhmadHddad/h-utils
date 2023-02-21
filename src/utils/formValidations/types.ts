c199b21f2cace48ddc5a4c6b162c65d216faa867
AhmadHddad <AhmadHddad@outlook.com>
2023-02-21 13:09:04 +0200

    deleting unessary stuff

--- src/utils/formValidations/types.ts
+++
@@ -1,3 +0,0 @@
-export type requiredValueProps = (errorMsg?: string | undefined) => (values: any) => string | void | Promise<string | void>;
-
-export type validateType = (value: string | undefined, errorMsg?: string) => string | undefined;
