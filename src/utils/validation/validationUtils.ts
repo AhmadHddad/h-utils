71dc9eccc1afcc62a189d136baefcb10c24a915f
AhmadHddad <AhmadHddad@outlook.com>
2023-02-21 13:07:51 +0200

    adding valiationUtils

---
+++ src/utils/validation/validationUtils.ts
@@ -0,0 +1,136 @@
+import { Key } from '../../types';
+import { count } from '../generalUtils/generalUtil';
+
+export const validateWebsite = (value, errorMsg = 'Invalid website') =>
+  value &&
+  !value.match(
+    /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi
+  )
+    ? errorMsg
+    : undefined;
+
+export const validateEmail = (value, errorMsg = 'Invalid email address') =>
+  value &&
+  !value.match(
+    /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  )
+    ? errorMsg
+    : undefined;
+
+/**
+ * @description checks if the passed value is null or undefined
+ * @example isNullOrUndefined("value") => false
+ * @example isNullOrUndefined(null) => true
+ * @param val unknown
+ * @returns boolean
+ */
+export const isNullOrUndefined = (val: unknown): val is null | undefined =>
+  val == null || val == undefined;
+
+/**
+ * @description If the value is undefined, return true, otherwise return false.
+ * @param {unknown} val - unknown - The value to check if it's undefined.
+ */
+export const isUndefined = (val: unknown): val is undefined => val == undefined;
+
+/**
+ * It checks if the value is null.
+ * @param {unknown} val - unknown
+ */
+export const isNull = (val: unknown): val is null => val == null;
+
+/**
+ * @description will convert passed params into object and return a new copy (not reference as Object.assign) of them merged to gother
+ * toObject({a:1},{b:2}) => {a:1, b:2}
+ * @param args objects
+ * @returns object
+ */
+
+export const toObject = <T>(...args: (T | undefined | {})[]) =>
+  args.reduce((prev, curr) => ({ ...(prev || {}), ...(curr || {}) }), {});
+
+/**
+ * @description If the source is null or undefined, return an empty array, otherwise return the source as an array.
+ * @param {unknown} source - unknown
+ * @returns An array.
+ */
+export function toArray(source: unknown) {
+  if (isNullOrUndefined(source)) return [];
+  return Array.isArray(source) ? source : [source];
+}
+
+/**
+ * @description The function isArray takes a value and returns true if the value is an array, otherwise it returns
+ * false.
+ * @param {any} value - unknown
+ */
+export const isArray = (value: unknown): value is [] => Array.isArray(value);
+
+/**
+ * @description  If the type of the value is a function, then return true, otherwise return false.
+ * @param {unknown} value - unknown
+ */
+export const isFunction = (value: unknown): value is (args: any) => any =>
+  typeof value === 'function';
+
+/**
+ * @description If the type of the value is a string, return true, otherwise return false.
+ * @param {unknown} value - unknown
+ */
+export const isString = (value: unknown): value is string =>
+  typeof value === 'string';
+
+/**
+ * @description If the type of the value is a object ({}), return true, otherwise return false.
+ * @param {unknown} value - unknown
+ */
+export const isObject = (val: unknown): val is {} =>
+  !!val && !Array.isArray(val) && typeof val === 'object';
+
+/**
+ * @description This function takes a value and returns true if the value is truthy, otherwise it returns false.
+ * @param {any} value - any
+ */
+export const isTruthy = (value: unknown): value is true => !!value;
+
+/**
+ * @description It returns true if the value is null, undefined, or an empty string
+ * @param {string | null} [value] - The value to check.
+ */
+export const isNullOrEmptyString = (value?: string | null) =>
+  isNullOrUndefined(value) || value?.trim().length === 0;
+
+/**
+ * @description If the value is not a number, then it is not a number.
+ * @param {any} value - any - The value to be checked
+ */
+export const isNumber = (value: any): value is number =>
+  !isNaN(value - parseFloat(value));
+
+/**
+ * @description  If the value is truthy and is a Date type, then return true, otherwise return false.
+ * @param {any} value - any
+ */
+export const isDateType = (value: any): value is Date =>
+  Boolean(value && value.getTime);
+
+/**
+ * @example isBiggerThan(1,2) => false
+ * @description The function isBiggerThan takes two arguments, a and b, and returns true if a is greater than b,
+ * otherwise it returns false.
+ */
+export const isBiggerThan = (a: Key, b: Key) => a > b;
+
+/**
+ * @example isSmallerThan(1,2) => true
+ * @description  It returns true if the first argument is smaller than the second argument
+ */
+export const isSmallerThan = (a: Key, b: Key) => a < b;
+
+/**
+ * @description It returns true if the object or array is not empty, false otherwise
+ * @returns A function that takes an unknown type and returns a boolean.
+ */
+export function isLength(input?: {} | [] | string) {
+  return count(input) > 0;
+}
