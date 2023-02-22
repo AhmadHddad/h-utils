ac91b99e9a201046a536f79b7a52f44384aa7877
AhmadHddad <AhmadHddad@outlook.com>
2023-02-22 12:05:11 +0200

    removing unessary utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,6 @@
 export * as arrayUtils from './array/arrayUtils';
 export * as stringUtils from './string/stringUtils';
-// export * as audioFileUtil from './audio/audioFileUtil';
 export * as generalUtil from './general/generalUtil';
-export * as domUtils from './dom/domUtils';
 export * as routeUtils from './route/routeUtils';
 export * as numberUtils from './number/numberUtils';
 export * as objectUtils from './object/objectUtils';
