278ac88620fe3662143ad1c900adcca078b8d032
Ahmad Naalweh <33379511+AhmadHddad@users.noreply.github.com>
2023-04-07 21:31:52 +0300

    adding animationInterval

---
+++ src/utils/browser/animationInterval.ts
@@ -0,0 +1,43 @@
+/**
+ * @description a reliable way to call a callback after a certain ms time.
+ * @example `// Usage
+const controller = new AbortController();
+
+// Create an animation callback every second:
+animationInterval(1000, controller.signal, time => {
+  console.log('tick!', time);
+});
+
+// And to stop it:
+controller.abort();`
+ */
+export default function animationInterval(
+  ms: number,
+  signal: AbortSignal,
+  callback: () => void
+) {
+  if (!isBroswer()) {
+    console.error("This util is for browsers only!");
+    return;
+  }
+
+  // Prefer currentTime, as it'll better sync animtions queued in the
+  // same frame, but if it isn't supported, performance.now() is fine.
+  const start = document?.timeline ? document?.timeline?.currentTime : performance.now();
+
+  function frame(time) {
+    if (signal.aborted) return;
+    callback(time);
+    scheduleFrame(time);
+  }
+
+  function scheduleFrame(time) {
+    const elapsed = time - start;
+    const roundedElapsed = Math.round(elapsed / ms) * ms;
+    const targetNext = start + roundedElapsed + ms;
+    const delay = targetNext - performance.now();
+    setTimeout(() => requestAnimationFrame(frame), delay);
+  }
+
+  scheduleFrame(start);
+}
