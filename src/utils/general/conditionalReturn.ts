0c76f1f03840fe1bae4065424d6dc0999e03ece2
AhmadHddad <AhmadHddad@outlook.com>
2023-04-29 17:44:42 +0300

    fixing condiational return

--- src/utils/general/conditionalReturn.ts
+++ src/utils/general/conditionalReturn.ts
@@ -5,7 +5,10 @@
  * @param [val2=null] - The value to return if the condition is false.
  * @example conditionalReturn([].length > 0, "has items") => if true will return "has items" if not will return val2 (null if not provided)
  */
-export default function conditionalReturn(condition: boolean, val1: any, val2 = null) {
-    condition ? val1 : val2;
-  }
-  
+export default function conditionalReturn<V1 = any, V2 = null>(
+  condition: boolean,
+  val1: V1,
+  val2 : V2 
+): V1 | V2 {
+  return condition ? val1 : val2;
+}
