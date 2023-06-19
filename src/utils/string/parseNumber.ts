d9ff30d0a34d2200e90f323206912bcfd601d1ca
Ahmad Naalweh <ahmadhddad@outlook.com>
2023-06-19 14:26:13 +0300

    type number check

--- src/utils/string/parseNumber.ts
+++ src/utils/string/parseNumber.ts
@@ -6,8 +6,10 @@ import isNumber from '../validation/isNumber';
  * @example parseNumber("123 ") => 123
  * @returns A function that takes a string and returns a string with all non-digit characters removed.
  */
-export default function parseNumber(number: string): number {
-  if (!number) return 0;
+export default function parseNumber(number: string | number): number {
+  if(!number) return 0;
+  if(isNumber(number)) return number;
+
   const parsedN = number.replace(/\D/g, '');
   return isNumber(parsedN) ? parsedN : 0;
 }
