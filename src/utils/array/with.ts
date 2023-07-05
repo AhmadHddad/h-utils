023b4ccd57872a12fb99cfc93a0206a746e38c83
AhmadHddad <AhmadHddad@outlook.com>
2023-07-05 16:56:35 +0300

    comment with

--- src/utils/array/with.ts
+++ src/utils/array/with.ts
@@ -1,20 +1,20 @@
-import navigateObjWithString from '../object/navigateObjWithString';
-import isArray from '../validation/isArray';
-import isFunction from '../validation/isFunction';
-import isNumber from '../validation/isNumber';
-import isObject from '../validation/isObject';
-import isString from '../validation/isString';
+// import navigateObjWithString from '../object/navigateObjWithString';
+// import isArray from '../validation/isArray';
+// import isNumber from '../validation/isNumber';
+// import isObject from '../validation/isObject';
+// import isString from '../validation/isString';
 
-export function withArr<T = any>(arr: T[], indexOrPredictor: number |string, replacer:T) {
-  if (!isArray(arr)) return arr;
+// export default function withArr<T = unknown>(arr: T[], indexOrPredictor: number |string, replacer:T) {
+//   if (!isArray(arr)) return arr;
 
-  if (isNumber(indexOrPredictor)) return arr.map((v, i) => (i === indexOrPredictor ? replacer : v));
+//   if (isNumber(indexOrPredictor)) return arr.map((v, i) => (i === indexOrPredictor ? replacer : v));
 
-  if(isString(indexOrPredictor)) return arr.map((v,i)=>{
-    if(isObject(v)) {
-      const obj = navigateObjWithString(v, indexOrPredictor);
+//   if(isString(indexOrPredictor)) return arr.map((v,i)=>{
+//     if(isObject(v)) {
+//       const obj = navigateObjWithString(v, indexOrPredictor);
       
-    }
-  })
+//     }
+//   })
 
-}
+//   return arr;
+// }
