36099b638bfcb38e2f04271d3b17281a44017409
AhmadHddad <AhmadHddad@outlook.com>
2023-07-06 16:00:30 +0300

    adding isIndex

---
+++ src/utils/validation/isIndex.ts
@@ -0,0 +1,33 @@
+import { Key } from '../types';
+import isNumber from './isNumber';
+
+/** Used as references for various `Number` constants. */
+const MAX_SAFE_INTEGER = 9007199254740991;
+
+/** Used to detect unsigned integer values. */
+const reIsUint = /^(?:0|[1-9]\d*)$/;
+
+/**
+ * Checks if `value` is a valid array-like index.
+ *
+ * @param {*} value The value to check.
+ * @param {number} [length=MAX_SAFE_INTEGER] The upper bounds of a valid index.
+ * @returns {boolean} Returns `true` if `value` is a valid index, else `false`.
+ */
+export default function isIndex(value: Key, length: number) {
+  value = Number(value);
+  if (!isNumber(value)) return false;
+
+  const type = typeof value;
+  length = length == null ? MAX_SAFE_INTEGER : length;
+
+  return (
+    !!length &&
+    (type === 'number' ||
+      (type !== 'symbol' && reIsUint.test(value?.toString()))) &&
+    value > -1 &&
+    value % 1 == 0 &&
+    value < length
+  );
+}
+
