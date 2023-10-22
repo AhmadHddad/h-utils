0221085974d6efe47311fb00037d4a22083c292b
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-10-22 15:06:45 +0300

    adding new utils to index

--- src/index.ts
+++ src/index.ts
@@ -4,32 +4,33 @@ export { default as HMap } from './extensions/HMap';
 // Array utils
 export { default as arrayRange } from './utils/array/arrayRange';
 export { default as removeDuplicatesFromArr } from './utils/array/removeDuplicatesFromArr';
-export { default as uniqueArray } from './utils/array/removeDuplicatesFromArr';
 export { default as sortArr } from './utils/array/sortArr';
 export { default as sortByMultiProperties } from './utils/array/sortByMultiProperties';
+export { default as uniqueArray } from './utils/array/removeDuplicatesFromArr';
 
 // Function util
-export { default as debounce } from './utils/functions/debounce';
-export { default as debouncePromise } from './utils/functions/debouncePromise';
-export { default as throttle } from './utils/functions/throttle';
-export { default as throttlePromise } from './utils/functions/throttlePromise';
-export { default as delay } from './utils/functions/delay';
-export { default as memoize } from './utils/functions/memoize';
 export { default as attempt } from './utils/functions/attempt';
 export { default as attemptAsync } from './utils/functions/attemptAsync';
+export { default as debounce } from './utils/functions/debounce';
+export { default as debouncePromise } from './utils/functions/debouncePromise';
+export { default as delay } from './utils/functions/delay';
+export { default as memoize } from './utils/functions/memoize';
+export { default as throttle } from './utils/functions/throttle';
+export { default as throttlePromise } from './utils/functions/throttlePromise';
 
 // General utils
 export { default as conditionalReturn } from './utils/general/conditionalReturn';
-export { default as onTimeout } from './utils/general/onTimeout';
 export { default as count } from './utils/general/count';
-export { default as toArray } from './utils/general/toArray';
-export { default as sleep } from './utils/general/sleep';
-export { default as wait } from './utils/general/sleep';
-export { default as noop } from './utils/general/noop';
+export { default as deepClone } from './utils/general/deepClone';
 export { default as generateGuid } from './utils/general/generateGuid';
 export { default as getGlobalThis } from './utils/general/getGlobalThis';
+export { default as getTag } from './utils/general/getTag';
+export { default as noop } from './utils/general/noop';
+export { default as onTimeout } from './utils/general/onTimeout';
+export { default as sleep } from './utils/general/sleep';
+export { default as toArray } from './utils/general/toArray';
 export { default as urlSearchParamsToObject } from './utils/general/urlSearchParamsToObject';
-export { default as deepClone } from './utils/general/deepClone';
+export { default as wait } from './utils/general/sleep';
 
 // File utils
 export { default as getBase64FromFile } from './utils/file/getBase64FromFile';
@@ -74,12 +75,12 @@ export { default as stringToPath } from './utils/string/stringToPath';
 export { default as compare2ObjectsBaseOnKeysArr } from './utils/object/compare2ObjectsBaseOnKeysArr';
 export { default as deepCompare } from './utils/object/deepCompare';
 export { default as excludeKeys } from './utils/object/excludeKeys';
-export { default as navigateObjWithString } from './utils/object/navigateObjWithString';
-export { default as includeKeys } from './utils/object/includeKeys';
-export { default as has } from './utils/object/has';
-export { default as joinObjects } from './utils/object/joinObjects';
 export { default as fastDeepEqual } from './utils/object/fastDeepEqual';
 export { default as getSmallestArrInObj } from './utils/object/getSmallestArrInObj';
+export { default as has } from './utils/object/has';
+export { default as includeKeys } from './utils/object/includeKeys';
+export { default as joinObjects } from './utils/object/joinObjects';
+export { default as navigateObjWithString } from './utils/object/navigateObjWithString';
 
 // Route utils
 export { default as addHttpToURL } from './utils/route/addHttpToURL';
@@ -95,42 +96,45 @@ export { default as stringifyUrl } from './utils/route/stringifyUrl';
 export { default as classNames } from './utils/style/classNames';
 
 // Validation utils
-export { default as isAsyncFunction } from './utils/validation/isAsyncFunction';
-export { default as isPromise } from './utils/validation/isPromise';
 export { default as isArray } from './utils/validation/isArray';
 export { default as isArrayOfStrings } from './utils/validation/isArrayOfStrings';
+export { default as isAsyncFunction } from './utils/validation/isAsyncFunction';
 export { default as isBiggerThan } from './utils/validation/isBiggerThan';
+export { default as isBoolean } from './utils/validation/isBoolean';
+export { default as isBrowser } from './utils/validation/isBrowser';
 export { default as isDateType } from './utils/validation/isDateType';
 export { default as isEmail } from './utils/validation/isEmail';
 export { default as isEmpty } from './utils/validation/isEmpty';
+export { default as isEqual } from './utils/validation/isEqual';
 export { default as isFunction } from './utils/validation/isFunction';
+export { default as isIndex } from './utils/validation/isIndex';
+export { default as isKeyInObject } from './utils/validation/isKeyInObject';
 export { default as isLength } from './utils/validation/isLength';
 export { default as isNull } from './utils/validation/isNull';
 export { default as isNullOrEmptyString } from './utils/validation/isNullOrEmptyString';
 export { default as isNullOrUndefined } from './utils/validation/isNullOrUndefined';
-export { default as isBoolean } from './utils/validation/isBoolean';
 export { default as isNumber } from './utils/validation/isNumber';
 export { default as isObject } from './utils/validation/isObject';
-export { default as isSmallerThan } from './utils/validation/isSmallerThan';
-export { default as isString } from './utils/validation/isString';
-export { default as isTruthy } from './utils/validation/isTruthy';
-export { default as isURL } from './utils/validation/isURL';
-export { default as isUndefined } from './utils/validation/isUndefined';
-export { default as isWhiteSpace } from './utils/validation/isWhiteSpace';
-export { default as isBrowser } from './utils/validation/isBrowser';
 export { default as isObjectLike } from './utils/validation/isObjectLike';
 export { default as isObjHasValues } from './utils/validation/isObjHasValues';
-export { default as isIndex } from './utils/validation/isIndex';
+export { default as isPromise } from './utils/validation/isPromise';
+export { default as isSmallerThan } from './utils/validation/isSmallerThan';
+export { default as isString } from './utils/validation/isString';
+export { default as isSymbol } from './utils/validation/isSymbol';
+export { default as isTruthy } from './utils/validation/isTruthy';
+export { default as isUndefined } from './utils/validation/isUndefined';
+export { default as isURL } from './utils/validation/isURL';
+export { default as isWhiteSpace } from './utils/validation/isWhiteSpace';
 
 // Browser utils
 export { default as addEventListener } from './utils/browser/addEventListener';
-export { default as removeEventListener } from './utils/browser/removeEventListener';
-export { default as getWindowObj } from './utils/browser/getWindowObj';
 export { default as animationInterval } from './utils/browser/animationInterval';
+export { default as getBrowser } from './utils/browser/getBrowser';
+export { default as getHtmlElementCoords } from './utils/browser/getHtmlElementCoords';
+export { default as getWindowObj } from './utils/browser/getWindowObj';
 export { default as onSetInterval } from './utils/browser/animationInterval';
 export { default as onUrlChange } from './utils/browser/onUrlChange';
-export { default as getHtmlElementCoords } from './utils/browser/getHtmlElementCoords';
-export { default as getBrowser } from './utils/browser/getBrowser';
+export { default as removeEventListener } from './utils/browser/removeEventListener';
 
 // Types
 export * from './utils/types';
