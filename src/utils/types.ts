3fb6c0858cf3f8949415b128955b8fcc8f0be345
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-10-22 21:17:41 +0300

    adding utilityTypes

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,18 +1,2 @@
-export type Key = string | number;
 export type Fn = (this: any, ...args: any[]) => any;
-
-export type Keys<T> = keyof T;
-export type Values<T> = T[Keys<T>];
-
-export type ExcludeFromArray<T extends any[], ToExclude> = Exclude<
-  T[number],
-  ToExclude
->[];
-
-export type PredicateFunc<T> = (
-  key: string | symbol,
-  value: string | Values<T>,
-  object: {} | object
-) => boolean;
-
-export type KeysArr = (string | symbol)[];
+export type {Falsy, KeysArr, PredicateFunc , Key,Primitive } from './utilityTypes';
