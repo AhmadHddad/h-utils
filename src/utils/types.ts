3a12d7572e91d9e97c24cca1f3b92feacd2bd511
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-10-23 15:01:54 +0300

    mobing types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,2 +1,8 @@
 export type Fn = (this: any, ...args: any[]) => any;
 export type {Falsy, KeysArr, PredicateFunc , Key,Primitive } from './utilityTypes';
+export type DebounceOptions = {
+    leading?: boolean;
+    trailing?: boolean;
+    maxWait?: number;
+  };
+  
