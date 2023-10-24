ffd5b93f193571182a3b12ec306eccac23843bc2
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-10-24 17:03:39 +0300

    checking for process

--- src/utils/validation/isDev.ts
+++ src/utils/validation/isDev.ts
@@ -2,6 +2,8 @@
  *@description will check if the current env is development
  */
 export default function isDev(): boolean {
+  if(!process || !process.env) return false;
+
   return (
     process?.env?.NODE_ENV === 'dev' ||
     process?.env?.NODE_ENV === 'development' ||
