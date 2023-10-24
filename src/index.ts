2a19c37e27d97f9735820c07ea5b2d3d69242c99
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-10-24 17:12:51 +0300

    adding getUrlObj

--- src/index.ts
+++ src/index.ts
@@ -85,6 +85,7 @@ export { default as navigateObjWithString } from './utils/object/navigateObjWith
 
 // Route utils
 export { default as addHttpToURL } from './utils/route/addHttpToURL';
+export { default as getUrlObj } from './utils/route/getUrlObj';
 export { default as excludeQueryParamFromUrl } from './utils/route/excludeQueryParamFromUrl';
 export { default as extractQueryFromUrl } from './utils/route/extractQueryFromUrl';
 export { default as parse } from './utils/route/parse';
---
+++ src/utils/route/getUrlObj.ts
@@ -0,0 +1,14 @@
+import addHttpToURL from './addHttpToURL';
+
+
+/**
+ * @description will make a new URL object for the passed url string.
+ * its like parseURL, more lightweight but with less functionalities.
+ */
+export default function getUrlObj(url: string): URL {
+  if (!(url.startsWith('http') || url.startsWith('https'))) {
+    url = addHttpToURL(url);
+  }
+
+  return new URL(url);
+}
