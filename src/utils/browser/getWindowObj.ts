83d05b1f2fb82d47976b17077f1b74edd79b259d
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-10-24 17:13:04 +0300

    fixing type issue and code enhancment

--- src/utils/browser/getWindowObj.ts
+++ src/utils/browser/getWindowObj.ts
@@ -1,5 +1,6 @@
+import getGlobalThis from "../general/getGlobalThis";
 import isBrowser from "../validation/isBrowser";
 
-export default function getWindowObj() : Window & typeof globalThis | {} {
-  return isBrowser() ? window : {};
+export default function getWindowObj() : Window | typeof globalThis {
+  return isBrowser() ? window : getGlobalThis();
 }
