ba295e0573de8b1e7ac2df6e21fc223dc91c00e3
Ahmad Naalweh <33379511+AhmadHddad@users.noreply.github.com>
2023-11-11 21:14:22 +0200

    fix type

--- src/utils/array/quickSort.ts
+++ src/utils/array/quickSort.ts
@@ -1,12 +1,11 @@
-
 /**
  * @description an implmentaion for quickSort algorightm, it will sort an array of numbers.
  */
-export default function quickSort(arr: number | string [] = []): number | string[] {
+export default function quickSort(arr: (number | string)[] = []): (number | string)[] {
   if (arr.length <= 1) return arr;
   let pivot = arr[0];
-  let leftArr: number | string[] = [];
-  let rightArr: number | string[] = [];
+  let leftArr: (number | string)[] = [];
+  let rightArr: (number | string)[] = [];
 
   for (let i = 1; i < arr.length; i++) {
     const item = arr[i];
