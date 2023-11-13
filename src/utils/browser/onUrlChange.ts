ee3d16d171bf43af0a03531da85028d160ae196f
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-11-13 17:54:54 +0200

    better interface for onUrlChange

--- src/utils/browser/onUrlChange.ts
+++ src/utils/browser/onUrlChange.ts
@@ -1,39 +1,33 @@
 import isBrowser from '../validation/isBrowser';
 
-
-type OnUrlChangeCallback =
-  | History['pushState']
-  | ((popstate: PopStateEvent) => void);
-
-type OnUrlChangeOptions = { onPush: boolean; onPop: boolean };
-
 /**
  * @function onUrlChange
  * @description -- For Browsers --
  *  a function that takes callback that will be called whenever the url is changed (ideal for SPA)
- * @param {OnUrlChangeCallback} cb
- * @param {OnUrlChangeOptions} options
+ * @param {History['pushState']} onPush
+ * @param {(popstate: PopStateEvent) => void} onPop
  */
 export default function onUrlChange(
-  cb: OnUrlChangeCallback,
-  options: OnUrlChangeOptions
+  onPush?: History['pushState'],
+  onPop?: (popstate: PopStateEvent) => void
 ) {
   if (!isBrowser()) return;
-
-  const { onPush = true, onPop = true } = options || {};
 
   if (onPush) {
     (function (history) {
       var pushState = history.pushState;
       history.pushState = function () {
         //@ts-ignore
-        cb(arguments);
+        onPush(...arguments);
         //@ts-ignore
         return pushState.apply(history, arguments);
       };
     })(window.history);
   }
   if (onPop) {
-    window.addEventListener('popstate', cb as ((popstate: PopStateEvent) => void));
+    window.addEventListener(
+      'popstate',
+      onPush as (popstate: PopStateEvent) => void
+    );
   }
 }
