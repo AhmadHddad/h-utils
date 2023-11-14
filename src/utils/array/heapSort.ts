d1c7328f8f41161239f40de716b9b9dfe2faf4b2
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-11-14 13:20:17 +0200

    rm log

--- src/utils/array/heapSort.ts
+++ src/utils/array/heapSort.ts
@@ -37,7 +37,3 @@ function heapify(arr: StringOrNumber[], n: number, i: number) {
     heapify(arr, n, largest);
   }
 }
-
-const arrayToSort = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
-const sortedArray = heapSort(arrayToSort);
-console.log(sortedArray);
