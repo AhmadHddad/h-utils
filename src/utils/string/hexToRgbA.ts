48d2d81681bff262fa1d939528e1f838a2708820
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-12-05 17:57:38 +0200

    checking if isHexColor

--- src/utils/string/hexToRgbA.ts
+++ src/utils/string/hexToRgbA.ts
@@ -1,21 +1,25 @@
 import { StringOrNumber } from '../types';
+import isHexColor from '../validation/isHexColor';
 
 /**
  * @description converts hex colors to rgba color.
  * @example hexToRgbA("#fff") // rgba(255,255,255,1)
  */
-export default function hexToRgbA(hex: string, alpha: StringOrNumber = 1): string {
-  let c;
-  if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
-    c = hex.substring(1).split('');
-    if (c.length == 3) {
-      c = [c[0], c[0], c[1], c[1], c[2], c[2]];
-    }
-    c = '0x' + c.join('');
-    return (
-      'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + `,${alpha})`
-    );
-  }
-  throw new Error('Bad Hex');
-}
+export default function hexToRgbA(
+  hex: string,
+  alpha: StringOrNumber = 1
+): string {
+  if (!isHexColor(hex)) throw new Error('Bad Hex');
 
+  let c;
+  c = hex.substring(1).split('');
+  if (c.length == 3) {
+    c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+  }
+  c = '0x' + c.join('');
+  return (
+    'rgba(' +
+    [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') +
+    `,${alpha})`
+  );
+}
