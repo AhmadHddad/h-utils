6923f88102b98e8040aba358707d88b7a8d484e8
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-12-05 18:58:36 +0200

    docs

--- src/utils/validation/isSmallerThan.ts
+++ src/utils/validation/isSmallerThan.ts
@@ -1,3 +1,9 @@
-import { Key } from '../types';
+import { Key } from '../utilityTypes';
 
-export default (a: Key, b: Key) => a < b;
+/**
+ * @example isSmallerThan(1,2) => true
+ * @description The function isSmallerThan takes two arguments, a and b, and returns true if a is smaller than b,
+ * otherwise it returns false.
+ */
+const isSmallerThan = (a: Key, b: Key) => a < b;
+export default isSmallerThan;
