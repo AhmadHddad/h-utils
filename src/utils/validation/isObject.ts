e1ce62e4e6a03db613253ccc9458e9dced9ac2ab
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-12-17 15:53:25 +0200

    passing T

--- src/utils/validation/isObject.ts
+++ src/utils/validation/isObject.ts
@@ -1,5 +1,6 @@
 /**
  * @description If the type of the value is a object, return true, otherwise return false.
  */
-const isObject = (val: unknown): val is {} => !!val && !Array.isArray(val) && typeof val === 'object';
+const isObject = <T>(val: unknown): val is T =>
+  !!val && !Array.isArray(val) && typeof val === 'object';
 export default isObject;
