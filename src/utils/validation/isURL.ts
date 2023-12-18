17c60c7ac8bc01c49a86cc04218618cea12eaa47
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-12-18 22:19:33 +0200

    adding docs and ability to try url

--- src/utils/validation/isURL.ts
+++ src/utils/validation/isURL.ts
@@ -1,6 +1,21 @@
-import { urlRegexForShortStr } from "../../regex/index";
+import { urlRegexForShortStr } from '../../regex/index';
 
+/**
+ * @description will check if the passed input is a valid url, if tryUrlObj will try to create a URL object
+ * if it was successful will return true, otherwise will return the result of the regex result.
+ * @example isURL("www.google.com") // true
+ * @example isURL("www.google.com", true) // false
+ */
+export default function isURL(input: string, tryUrlObj?: boolean): boolean {
+  let res = !!input.match(urlRegexForShortStr);
 
-export default function isURL(input: string): boolean {
-  return !!input.match(urlRegexForShortStr);
+  if (tryUrlObj) {
+    try {
+      res = !!new URL(input);
+    } catch (error) {
+      res = false;
+    }
+  }
+
+  return res;
 }
