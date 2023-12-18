c0eb869585e5af2b0e6f7e8b5ecc55841724eea8
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2023-12-18 22:21:11 +0200

    fix obj

--- src/utils/validation/isObject.ts
+++ src/utils/validation/isObject.ts
@@ -1,6 +1,6 @@
 /**
  * @description If the type of the value is a object, return true, otherwise return false.
  */
-const isObject = <T>(val: unknown): val is T =>
+const isObject = <T = {}>(val: unknown): val is T =>
   !!val && !Array.isArray(val) && typeof val === 'object';
 export default isObject;
