485828ecbb7e55a64b61e0a575832146ece222d9
Ahmad Naalweh <33379511+AhmadHddad@users.noreply.github.com>
2024-01-19 17:35:42 +0200

    adding docs

--- src/utils/functions/pipe.ts
+++ src/utils/functions/pipe.ts
@@ -1,5 +1,19 @@
 import { PipeFunction } from "../types";
 
+/**
+ * @example 
+ * 
+// Functions to be piped
+const addTwo = (x: number) => x + 2;
+const square = (x: number) => x * x;
+const toString = (x: number) => x.toString();
+
+// Pipe the functions
+const pipeline = pipe(addTwo, square, toString);
+
+// Use the pipeline
+const result = pipeline(3);
+ */
 export default function pipe<T>(...fns: PipeFunction<any, any>[]): (input: T) => any {
   return (input: T) => fns.reduce((result, fn) => fn(result), input);
 }
