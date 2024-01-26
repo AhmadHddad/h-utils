4c42fea2e99622da9112322b3682e77ea4a72a60
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2024-01-26 20:14:18 +0200

    fix code and debrecate isKeyInObject

--- src/utils/validation/isKeyInObject.ts
+++ src/utils/validation/isKeyInObject.ts
@@ -1,34 +1,12 @@
-import isSymbol from "./isSymbol";
-
-/** Used to match property names within property paths. */
-const reIsDeepProp = /\.|\[(?:[^[\]]*|(["'])(?:(?!\1)[^\\]|\\.)*?\1)\]/;
-const reIsPlainProp = /^\w*$/;
+import has from "../object/has";
 
 /**
- * Checks if `value` is a property name and not a property path.
- *
- * @param {*} value The value to check.
- * @param {Object} [object] The object to query keys on.
- * @returns {boolean} Returns `true` if `value` is a property name, else `false`.
+ * @description checks if the passed key in the object.
+ * @deprecated use has instead.
  */
-function isKeyInObject(value, object) {
-  if (Array.isArray(value)) {
-    return false;
-  }
-  const type = typeof value;
-  if (
-    type === 'number' ||
-    type === 'boolean' ||
-    value == null ||
-    isSymbol(value)
-  ) {
-    return true;
-  }
-  return (
-    reIsPlainProp.test(value) ||
-    !reIsDeepProp.test(value) ||
-    (object != null && value in Object(object))
-  );
+export default function isKeyInObject(
+  objectType: {} | Function | [],
+  key: string
+): boolean {
+  return has(objectType, key);
 }
-
-export default isKeyInObject;
