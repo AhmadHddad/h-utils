7de039e6a2c3c1d6b470cdb6b38d94e53dd8c46b
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2024-02-26 13:30:32 +0200

    adding HttpStatusCode constants

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ export { default as EventBus } from './classes/EventBus';
 export { default as TwoWayBinder } from './classes/TwoWayBinder';
 export { default as DOMEventBus } from './classes/DOMEventBus';
 
+
+// constants
+export { default as HttpStatusCode } from './constants/HttpStatusCode';
+
 // Array utils
 export { default as arrayRange } from './utils/array/arrayRange';
 export { default as bubbleSort } from './utils/array/bubbleSort';
