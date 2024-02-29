c1052fabbaac585c4043f75e27c01a72981f4cc7
AhmadHddad <33379511+AhmadHddad@users.noreply.github.com>
2024-02-29 12:14:27 +0200

    rename and fix implmentaiton

--- src/classes/ConsoleRecorder.ts
+++ src/classes/ConsoleRecorder.ts
@@ -1,13 +1,24 @@
-import { LogRecord } from "../utils/types";
+// import { ConsoleRecord } from '../utils/types';
+export type ConsoleRecordKeys = 'error' | 'log' | 'info' | 'table' | 'warn';
+export type ConsoleRecord = Record<
+  ConsoleRecordKeys,
+  { time: string; value: any[] }[]
+>;
 
 /**
  * @description A utility to record all logs 'error' | 'log' | 'info' | 'table' | 'warn', will save them to an object logRecord with time.
- * @example const logManager = new LogManager();
+ * @example const logManager = new ConsoleRecorder();
  * console.log("foo");
  * logManager.logRecord.log // [{time:"123", value:["foo"]}]
  */
-export default class LogManager {
-  private _logRecord: LogRecord = {
+export default class ConsoleRecorder {
+  private oldestLog = console.log;
+  private oldestInfo = console.info;
+  private oldestError = console.error;
+  private oldestWarn = console.warn;
+  private oldestTable = console.table;
+
+  private _logRecord: ConsoleRecord = {
     error: [],
     info: [],
     log: [],
@@ -15,14 +26,31 @@ export default class LogManager {
     warn: [],
   };
 
-  constructor() {
-    const oldLog = console.log;
-    const oldInfo = console.info;
-    const oldError = console.error;
-    const oldTable = console.table;
-    const oldWarn = console.warn;
-    const that = this;
+  constructor(options?: {
+    error?: boolean;
+    info?: boolean;
+    log?: boolean;
+    table?: boolean;
+    warn?: boolean;
+  }) {
+    const {
+      error = true,
+      info = true,
+      table = true,
+      log = true,
+      warn = true,
+    } = options || {};
 
+    error && this.recordError();
+    info && this.recordInfo();
+    log && this.recordLog();
+    table && this.recordTable();
+    warn && this.recordWarn();
+  }
+
+  recordLog = () => {
+    const oldLog = console.log;
+    const that = this;
     console.log = function () {
       //@ts-ignore
       that._logRecord.log.push({
@@ -32,7 +60,11 @@ export default class LogManager {
       //@ts-ignore
       return oldLog.apply(console, arguments);
     };
+  };
 
+  recordError = () => {
+    const oldError = console.error;
+    const that = this;
     console.error = function () {
       //@ts-ignore
       that._logRecord.error.push({
@@ -42,7 +74,11 @@ export default class LogManager {
       //@ts-ignore
       return oldError.apply(console, arguments);
     };
+  };
 
+  recordInfo = () => {
+    const oldInfo = console.info;
+    const that = this;
     console.info = function () {
       //@ts-ignore
       that._logRecord.info.push({
@@ -52,7 +88,11 @@ export default class LogManager {
       //@ts-ignore
       return oldInfo.apply(console, arguments);
     };
+  };
 
+  recordWarn = () => {
+    const oldWarn = console.warn;
+    const that = this;
     console.warn = function () {
       //@ts-ignore
       that._logRecord.warn.push({
@@ -62,7 +102,11 @@ export default class LogManager {
       //@ts-ignore
       return oldWarn.apply(console, arguments);
     };
+  };
 
+  recordTable = () => {
+    const oldTable = console.table;
+    const that = this;
     console.table = function () {
       //@ts-ignore
       that._logRecord.table.push({
@@ -72,9 +116,17 @@ export default class LogManager {
       //@ts-ignore
       return oldTable.apply(console, arguments);
     };
-  }
+  };
 
   get logRecord() {
     return this._logRecord;
   }
+
+  stopAllRecord = () => {
+    console.error = this.oldestError;
+    console.info = this.oldestInfo;
+    console.log = this.oldestLog;
+    console.warn = this.oldestWarn;
+    console.table = this.oldestTable;
+  };
 }
