1ccd4bd9487eff5babe04d47b4abe4467ad3670c
AhmadHddad <AhmadHddad@outlook.com>
2024-08-03 13:49:42 +0300

    fix: spead

--- src/utils/object/joinObjects.ts
+++ src/utils/object/joinObjects.ts
@@ -1,28 +1,24 @@
-import isObject from '../validation/isObject';
-
 /**
  * @description will return a new object that combines all of the passed objects as new copy (not reference as Object.assign).
  * @example joinObjects({a:1},{b:2}) => {a:1, b:2}
+ * @example joinObjects([1,2]) => {}
  * @example joinObjects(({ a: 1 }, { b: 2 })) => { a: 1, b: 2 }
  * @example joinObjects(({ a: 1 }, null || undefined || !object)) => { a: 1 }
  */
-function joinObjects<T extends object | null | undefined | {}>(
+export default function joinObjects<T extends object | null | undefined | {}>(
   ...args: (T | any)[]
 ): T {
-  const newObj: Record<string, any> = {};
+  let newObj: Record<string, any> = {};
 
   for (let index = 0; index < args.length; index++) {
     const object = args[index];
-    if (!isObject(object)) continue;
 
-    for (const key in object) {
-      if (Object.prototype.hasOwnProperty.call(object, key)) {
-        newObj[key] = object[key as string];
-      }
+    if (!!object && typeof object === 'object' && !Array.isArray(object)) {
+      newObj = { ...newObj, ...object };
+    } else {
+      continue;
     }
   }
 
   return newObj as T;
 }
-
-export default joinObjects;
