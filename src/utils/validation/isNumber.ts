898d74df83c2a2288dd155318502aa9fe8999c4a
AhmadHddad <AhmadHddad@outlook.com>
2024-09-12 14:37:44 +0200

    fix isNumber docs

--- src/utils/validation/isNumber.ts
+++ src/utils/validation/isNumber.ts
@@ -1,10 +1,11 @@
 /**
  * @description If the value is not a number, then it is not a number.
  * @example isNumber("123") => false;
- * @example isNumber("123", true) => false;
+ * @example isNumber("123", true) => true;
  * @example isNumber(1, true) => true;
  */
 export default function isNumber(value: any, sloppy?: boolean): value is number {
   let res = !isNaN(value - parseFloat(value));
   return !sloppy ? typeof value === "number" && res : res;
 }
+
