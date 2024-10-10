8e9e542f98b1a43a336ce585dc8666b21b0e894d
AhmadHddad <AhmadHddad@outlook.com>
2024-10-10 12:02:22 +0200

    fix doc

--- src/utils/general/toArray.ts
+++ src/utils/general/toArray.ts
@@ -2,7 +2,7 @@
 /**
  * @description If the source is null or undefined, return an empty array, otherwise return the source as an array.
  * @example toArray(null) // []
- * @example toArray([21]) // []
+ * @example toArray([21]) // [21]
  * @example toArray({}) // [{}]
  * @param {unknown} source - unknown
  * @returns An array.
@@ -11,4 +11,5 @@ export default function toArray(source: unknown) {
     if (!source) return [];
     return Array.isArray(source) ? source : [source];
   }
+
   
